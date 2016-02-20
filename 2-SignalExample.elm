b621a466f4d5808308c3bae2bc064e478846c3a0
David G Chaves <davidgchaves@gmail.com>
2016-02-20 18:26:30 +0100

    Map over the Keyboard.wasd Signal

--- 2-SignalExample.elm
+++ 2-SignalExample.elm
@@ -1,7 +1,14 @@
-import Graphics.Element exposing (..)
+import Graphics.Element exposing (show)
 import Mouse
 import Window
 import Keyboard
 import Time
 
 main =
+  -- show: a -> Element
+  -- map: (a -> result) -> Signal a -> Signal result
+
+  -- Keyboard.wasd : Signal { x : Int, y : Int }
+  -- show: { x: Int, y: Int } -> Element
+  -- map: ({ x: Int, y: Int } -> Element) -> Signal { x: Int, y: Int } -> Signal Element
+  Signal.map show Keyboard.wasd
