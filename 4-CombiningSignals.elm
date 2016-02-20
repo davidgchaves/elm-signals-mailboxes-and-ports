02c6e88746c2ecaa1b6c1355d25a44993e894db5
David G Chaves <davidgchaves@gmail.com>
2016-02-20 20:07:10 +0100

    Map2 over 2 different signals

--- 4-CombiningSignals.elm
+++ 4-CombiningSignals.elm
@@ -3,5 +3,28 @@ import Mouse
 import Window
 
 
+leftOrRight : Int -> Int -> Element
+leftOrRight x width =
+  let
+    middle = width // 2
+    side   = if x > middle then "Right" else "Left"
+  in
+    show side
+
+
+topOrBottom : Int -> Int -> Element
+topOrBottom y height =
+  let
+    middle = height // 2
+    side   = if y > middle then "Bottom" else "Top"
+  in
+    show side
+
+-- Signal.map2 : (a -> b -> result) -> Signal a -> Signal b -> Signal result
+-- Mouse.x : Signal Int
+-- Mouse.y : Signal Int
+-- Window.width : Signal Int
+-- Window.height : Signal Int
 main : Signal Element
 main =
+  Signal.map2 topOrBottom Mouse.y Window.height
