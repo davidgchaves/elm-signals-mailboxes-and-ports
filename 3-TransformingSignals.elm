7fb00176a63a248bcc5f8f9911c3374c466716f9
David G Chaves <davidgchaves@gmail.com>
2016-02-20 19:27:58 +0100

    Add 2 examples of transforming signals

--- 3-TransformingSignals.elm
+++ 3-TransformingSignals.elm
@@ -2,5 +2,24 @@ import Graphics.Element exposing (..)
 import Mouse
 import Window
 import Keyboard
+import Char
 
+area : (Int, Int) -> Int
+area (w,h) = w * h
+
+
+-- Signal KeyCode |> Signal Char |> Signal Bool |> Signal Element
+main : Signal Element
 main =
+  Keyboard.presses
+    |> Signal.map Char.fromCode
+    |> Signal.map Char.isDigit
+    |> Signal.map show
+
+
+-- Signal (Int,Int) |> Signal Int |> Signal Element
+-- main : Signal Element
+-- main =
+--   Window.dimensions
+--     |> Signal.map area
+--     |> Signal.map show
