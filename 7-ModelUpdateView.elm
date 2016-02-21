86b611211da10cdface4e8714c24c2437fa26d94
David G Chaves <davidgchaves@gmail.com>
2016-02-21 19:25:02 +0100

    Refactor towards a Signal Model

--- 7-ModelUpdateView.elm
+++ 7-ModelUpdateView.elm
@@ -2,11 +2,19 @@ import Graphics.Element exposing (Element, show)
 import Mouse            exposing (clicks)
 
 
-state : Signal Int
-state =
-  Signal.foldp (\_ count -> count + 1) 0 Mouse.clicks
+-- MODEL
+type alias Model = Int
+
+
+initialModel : Model
+initialModel = 0
+
+
+model : Signal Model
+model =
+  Signal.foldp (\_ count -> count + 1) initialModel Mouse.clicks
 
 
 main : Signal Element
 main =
-  Signal.map Graphics.Element.show state
+  Signal.map Graphics.Element.show model
