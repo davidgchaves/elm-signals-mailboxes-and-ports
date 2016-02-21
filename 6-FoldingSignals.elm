17dac43919e536008ea0040a107f35d0c765ae1d
David G Chaves <davidgchaves@gmail.com>
2016-02-21 17:41:41 +0100

    Count the number of key pressed using a past-dependant signal

--- 6-FoldingSignals.elm
+++ 6-FoldingSignals.elm
@@ -1,7 +1,12 @@
-import Graphics.Element exposing (..)
-import Keyboard
-import Mouse
+import Graphics.Element exposing (Element, show)
+import Keyboard         exposing (presses)
 
 
+-- Signal KeyCode
+--   |> Signal Int (past dependant signal -- accumulate the number of key presses)
+--   |> Signal ELement
 main : Signal Element
 main =
+  Keyboard.presses
+    |> Signal.foldp (\_ count -> count + 1) 0 -- STATE!
+    |> Signal.map Graphics.Element.show
