aa4cdca21e1fe3d490e18adc7c117a794d3173c5
David G Chaves <davidgchaves@gmail.com>
2016-02-21 10:24:23 +0100

    Get the position where the user clicks

--- 5-SamplingSignals.elm
+++ 5-SamplingSignals.elm
@@ -3,5 +3,9 @@ import Mouse
 import Time
 
 
+-- Signal (Int, Int) |> Signal (Int, Int) |> Signal Element
 main : Signal Element
 main =
+  Mouse.position
+    |> Signal.sampleOn Mouse.clicks
+    |> Signal.map show
