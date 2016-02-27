334bf009e21af499fe65f3725664639528e9028d
David G Chaves <davidgchaves@gmail.com>
2016-02-27 11:28:57 +0100

    Fullscreen follow the mouse

--- 8b-MouseTracker.elm
+++ 8b-MouseTracker.elm
@@ -12,17 +12,16 @@ drawCircle x y =
     |> move (x, y)
 
 
-view : (Int, Int) -> Element
-view (mouseX, mouseY) =
+view : (Int, Int) -> (Int, Int) -> Element
+view (windowW, windowH) (mouseX, mouseY) =
   let
-    (w, h) = (300, 300)
-    (w', h') = (toFloat w, toFloat h)
+    (w', h') = (toFloat windowW, toFloat windowH)
     x = toFloat mouseX - (w' / 2)
     y = (h' / 2) - toFloat mouseY
   in
-    collage w h [ drawCircle x y ]
+    collage windowW windowH [ drawCircle x y ]
 
 
 main : Signal Element
 main =
-  Signal.map view Mouse.position
+  Signal.map2 view Window.dimensions Mouse.position
