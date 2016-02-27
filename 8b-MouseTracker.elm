31cb2a92ca526540e784b449627ab55c789041fc
David G Chaves <davidgchaves@gmail.com>
2016-02-27 11:22:15 +0100

    Add MouseTracker starting point

---
+++ 8b-MouseTracker.elm
@@ -0,0 +1,28 @@
+import Graphics.Element exposing (..)
+import Graphics.Collage exposing (..)
+import Color exposing (..)
+import Mouse
+import Window
+
+
+drawCircle : Float -> Float -> Form
+drawCircle x y =
+  circle 50
+    |> filled red
+    |> move (x, y)
+
+
+view : (Int, Int) -> Element
+view (mouseX, mouseY) =
+  let
+    (w, h) = (300, 300)
+    (w', h') = (toFloat w, toFloat h)
+    x = toFloat mouseX - (w' / 2)
+    y = (h' / 2) - toFloat mouseY
+  in
+    collage w h [ drawCircle x y ]
+
+
+main : Element
+main =
+  view (100, 100)
