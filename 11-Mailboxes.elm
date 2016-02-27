4231ca00299f57d6cee1c9076b9aac5b2456c60e
David G Chaves <davidgchaves@gmail.com>
2016-02-27 18:26:21 +0100

    Wire a button and a mailbox

--- 11-Mailboxes.elm
+++ 11-Mailboxes.elm
@@ -5,11 +5,23 @@ import Html.Events exposing (..)
 view : String -> Html
 view greeting =
   div []
-    [ button [  ] [ text "Click for English" ],
+    [ button
+        [ on "click" targetValue (\_ -> Signal.message inbox.address "Hello") ]
+        [ text "Click for English" ],
       p [ ] [ text greeting ]
     ]
 
 
-main : Html
+inbox : Signal.Mailbox String
+inbox =
+  Signal.mailbox "Waiting...."
+
+
+messages : Signal String
+messages =
+  inbox.signal
+
+
+main : Signal Html
 main =
-  view "- - - -"
+  Signal.map view messages
